5945f660ec2c2d4ff410c7a054fd782f3f86bd54
nadiacomanici <nadi_comanici@yahoo.com>
2020-05-09 09:33:38 +0300

    no message

---
+++ BddDemo.PageObjects/BddDemo.Specflow/BeforeAfter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using TechTalk.SpecFlow;
+
+namespace BddDemo.Specflow
+{
+    public class BeforeAfter
+    {
+        private IWebDriver _driver;
+        public IWebDriver Driver { get => _driver; set => _driver = value; }
+
+        [BeforeScenario()]
+        public void BeforeScenario()
+        {
+            Driver = new ChromeDriver();
+            Driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(2);
+            Driver.Manage().Window.Maximize();
+            Driver.Navigate().GoToUrl("http://automationpractice.com");
+        }
+
+        [AfterScenario()]
+        public void AfterScenario()
+        {
+            Driver.Close();
+            Driver.Quit();
+        }
+    }
+}
---
+++ BddDemo.PageObjects/BddDemo.Specflow/StepDefinitions/SignUpSteps.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BddDemo.PageObjects.PageObjects;
+using NUnit.Framework.Internal;
+using OpenQA.Selenium;
+using TechTalk.SpecFlow;
+
+namespace BddDemo.Specflow
+{
+    [Binding]
+    public sealed class BasicSteps:BeforeAfter
+    {
+        private readonly ScenarioContext _context;
+        public BasicSteps(ScenarioContext injectedContext)
+        {
+            _context = injectedContext;
+        }
+
+        [Given(@"I navigate to ""(.*)""")]
+        public void GivenINavigateTo(string p0)
+        {
+           
+        }
+
+    }
+}
