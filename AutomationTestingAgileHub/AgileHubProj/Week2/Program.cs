d4c92cdb15f8979e103c5005682fba67345b10fb
Flitar Radu <radu.flitar@ipsos.com>
2020-10-05 20:35:45 +0300

    week 2 - Monday

--- AutomationTestingAgileHub/AgileHubProj/Week2/Program.cs
+++ AutomationTestingAgileHub/AgileHubProj/Week2/Program.cs
@@ -6,7 +6,61 @@ namespace Week2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // User details
+
+            //var firstName = "Radu";
+            //string lastName = "Flitar";
+            //var city = "Brasov";
+            //int age = 31;
+            //string address;
+
+            //Console.WriteLine(" \nEnter your name:");
+            //firstName = Console.ReadLine();
+            //Console.WriteLine(" \nEnter your lastname: ");
+            //lastName = Console.ReadLine();
+
+            //Console.WriteLine(" \nEnter your city:  ");
+            //city = Console.ReadLine();
+
+            //Console.WriteLine(" \nEnter your age:  ");
+            //age = Int32.Parse(Console.ReadLine());
+
+            //Console.WriteLine(" \nEnter your address:  ");
+            //address = Console.ReadLine();
+
+            //// Output values
+
+            //Console.WriteLine("====================================");
+            //Console.WriteLine("========= User details: ============");
+            //Console.WriteLine("====================================");
+
+            //Console.WriteLine($"Name: {firstName} {lastName}");
+            //Console.WriteLine($"Age: {age}");
+            //Console.WriteLine($"City: {city}");
+            //Console.WriteLine($"Address: {address}");
+
+            //Console.ReadKey();
+
+            // Exrcise 2
+
+            string firstName;
+            string lastName;
+            string secondLastName = "radu";
+
+            Console.WriteLine(" Enter your name:");
+            firstName = Console.ReadLine();
+            Console.WriteLine(" \nEnter your lastname: ");
+            lastName = Console.ReadLine();
+
+            // Compare strings
+
+            Console.WriteLine(firstName.CompareTo(secondLastName));
+
+
+            Console.ReadKey();
+
+
+
         }
     }
 }
