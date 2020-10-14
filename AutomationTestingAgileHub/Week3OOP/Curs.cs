8c4354fb5030bb0019d36a16f1a103a37976dbc6
Flitar Radu <radu.flitar@ipsos.com>
2020-10-14 20:37:52 +0300

    Week3day2
    OOP

---
+++ AutomationTestingAgileHub/Week3OOP/Curs.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Week3OOP
+{
+    class Curs
+    {
+
+        private int CursId { get; set; }
+        private string Nume { get; set; }
+        private int Durata { get; set; }
+
+        //public Curs(int id, string nume, int durata)
+        //{
+        //    CursId = id;
+        //    Nume = nume;
+        //    Durata = durata;
+        //}
+
+        //public Curs(int id, string nume)
+        //{
+        //    CursId = id;
+        //    Nume = nume;
+        //    Durata = 12;
+        //}
+
+
+        public void SetCurs(int id, string nume, int durata)
+        {
+            CursId = id;
+            Nume = nume;
+            Durata = durata;
+        }
+    }
+}
---
+++ AutomationTestingAgileHub/Week3OOP/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Week3OOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Curs cursulMeu = new Curs(1, "QAA", 48);
+
+            //var cursulMeu2 = new Curs(2, "FrontEnt");
+
+            var tutorial = new Tutorial();
+            tutorial.SetCurs(1, "OOP", 2);
+
+            Console.ReadKey();
+        }
+    }
+}
