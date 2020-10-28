32994ec482fe7807e6acdb4bdf89fd2d19f3c8d2
Flitar Radu <radu.flitar@ipsos.com>
2020-10-28 20:31:56 +0200

    Week5SpecFlow

--- AutomationTestingAgileHub/SeleniumTestProject/Hooks.cs
+++ AutomationTestingAgileHub/SeleniumTestProject/Hooks.cs
@@ -1,6 +1,8 @@
 ﻿using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Remote;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,20 +11,42 @@ using System.Threading.Tasks;
 
 namespace SeleniumTestProject
 {
+    public enum BrowserType
+    {
+        Chrome,
+        Firefox
+    }
     public class Hooks
     {
-        protected IWebDriver Driver; 
+        private BrowserType _browserType;
+        protected IWebDriver Driver;
+
         [SetUp]
         public void Setup()
         {
-            Driver = new ChromeDriver();
-                        // setam driverul sa stea cate o secunda pentru fiecere element unde e folosit
-            //Driver.Manage().Timeouts().ImplicitWait = TimeSpan
-            //.FromSeconds(2);
+
+            var browserType = TestContext.Parameters.Get("Browser", "Chrome");
+            _browserType = (BrowserType)Enum.Parse(typeof(BrowserType), browserType);
+            ChooseDriverInstance(_browserType);
             Driver.Manage().Window.Maximize();
-
-
             Driver.Navigate().GoToUrl("http://demosite.casqad.org/");
+        }
+
+        public void ChooseDriverInstance(BrowserType browserType)
+        {
+            switch (browserType)
+            {
+                case BrowserType.Chrome:
+                    //var optionsChrome = new ChromeOptions();
+                    //Driver = new RemoteWebDriver(new Uri(" http://localhost:4446/wd/hub"), optionsChrome);
+                    Driver = new ChromeDriver();
+                    break;
+                case BrowserType.Firefox:
+                    //var optionsFirefox = new FirefoxOptions();
+                    //Driver = new RemoteWebDriver(new Uri(" http://localhost:4446/wd/hub"), optionsFirefox);
+                    Driver = new FirefoxDriver();
+                    break;
+            }
         }
 
         [TearDown]
