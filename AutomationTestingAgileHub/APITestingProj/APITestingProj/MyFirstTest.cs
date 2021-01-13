abbd0e960336a75ef42d419eb11bf55e865f5a57
Flitar Radu <radu.flitar@ipsos.com>
2021-01-13 20:34:53 +0200

    API Testing in CSharp

---
+++ AutomationTestingAgileHub/APITestingProj/APITestingProj/MyFirstTest.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Newtonsoft.Json.Linq;
+using NUnit;
+using NUnit.Framework;
+using RestSharp;
+
+namespace APITestingProj
+{
+    [TestFixture]
+    public class MyFirstTest
+    {
+        [Test]
+        public void ValidateGetBookingRequest()
+        {
+            var zaBookingId = "10";
+            // create Client Connection
+            RestClient restClient = new RestClient($"https://restful-booker.herokuapp.com/booking/{zaBookingId}");
+
+            // Create request get data
+            RestRequest restRequest = new RestRequest(Method.GET);
+            restRequest = restRequest.AddHeader("Accept", "application/json") as RestRequest;
+
+            // Execute request to server
+            IRestResponse response = restClient.Execute(restRequest);
+
+            // Extract data from response
+            var responsData = response.Content;
+
+            // Assert
+            Assert.IsTrue(responsData.Contains("Sally"));
+
+
+        }
+
+        [Test]
+        public void ValidatePostRequest()
+        {
+            // create Client Connection
+            RestClient restClient = new RestClient($"https://restful-booker.herokuapp.com/auth");
+
+            JObject objectBody = new JObject();
+            objectBody.Add("username", "admin");
+            objectBody.Add("password", "password123");
+
+            var restRequest = new RestRequest(Method.POST);
+            restRequest.AddParameter("application/json", objectBody, ParameterType.RequestBody);
+
+            var response = restClient.Execute(restRequest);
+
+            Assert.IsTrue(response.Content.Contains("token"));
+            Assert.IsTrue(response.StatusCode.ToString().Contains("OK"));
+
+        }
+    }
+}
---
+++ AutomationTestingAgileHub/APITestingProj/APITestingProj/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("APITestingProj")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("APITestingProj")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("3bbc5718-5979-403f-9873-0e94e09ba871")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
